e7774df047595d92d9b9ddfd5a66f18179eba132
fhftmdfl <cjh5425@gmail.com>
2026-03-05 19:31:44 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  reactStrictMode: true,
+  basePath: "/wedding-invitation",
+  assetPrefix: "/wedding-invitation/",
 };
 
 export default nextConfig;
